c52e01eeecfec10903820043281f04b2cf978444
multitask-builder <builder@local>
2026-09-24 00:36:00 +0800

    fix(multitask): PiP 协议 skip 方法使用 SDK 重命名后的 completion 标签

--- MultitaskSupport/MultitaskDockView.swift
+++ MultitaskSupport/MultitaskDockView.swift
@@ -530,7 +530,7 @@ extension StagePiPKeepAlive: AVPictureInPictureSampleBufferPlaybackDelegate {
 
     func pictureInPictureController(_ controller: AVPictureInPictureController,
                                     skipByInterval skipInterval: CMTime,
-                                    completionHandler: @escaping () -> Void) {
+                                    completion completionHandler: @escaping () -> Void) {
         completionHandler()
     }
 }
